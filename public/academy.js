9eae97010135e74d3d7a1d17b99258ff29c4cebe
Manon Sanoi <info@manonsanoi.com>
2022-05-10 22:57:55 +0700

    css theme

--- public/academy.js
+++ public/academy.js
@@ -870,7 +870,7 @@ function getFirebasePlayer()
                     +"<th class='p-3'>"
                         +"<div class='align-items-center'>"
                             +"<i class='" + item.typeicon + " h6'></i>"
-                            +"<p class='mb-0 d-inline fw-normal h6 ms-1'><a href='play.html?token=" + item.uid + "&session=" + item.course + "' class='' title='" + item.title + "' data-bs-toggle='tooltip' data-bs-placement='top'>" + item.title + "</a></p>"
+                            +"<p class='mb-0 d-inline fw-normal topic-name-list h6 ms-1'><a href='play.html?token=" + item.uid + "&session=" + item.course + "' class='' title='" + item.title + "' data-bs-toggle='tooltip' data-bs-placement='top'>" + item.title + "</a></p>"
                             +"<p class='mb-0 d-inline fw-normal topic-duration-badge'> <i class='uil uil-clock'></i> " + item.duration + " </p>"
                         +"</div>"
                     +"</th>"
